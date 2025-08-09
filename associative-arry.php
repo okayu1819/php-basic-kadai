0341e6c58184495525ccb535feabcc5d95cddd53
okayu1819 <yktruth15@gmail.com>
2025-08-09 16:56:44 +0900

    first commit kadai007

---
+++ associative-arry.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>連想配列を作ってキーと値を出力しよう</title>
+</head>
+<body>
+    <p>
+        <?php
+        // 連想配列を作成
+        $vesitable_data = [
+            "name" => "onion",
+            "price" => 200,
+            "weight" => 160,
+        ];
+        // キーと値を出力
+        print_r($vesitable_data);
+        ?>
+    </p>
+</body>
+</html>
